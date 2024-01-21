007054f7612a00f693443468fead535a4b4da358
Даниил <danildergachevfox@gmail.com>
2024-01-21 12:55:44 +0300

    complete news screen

--- client/app/screens/News/NewsScreen.tsx
+++ client/app/screens/News/NewsScreen.tsx
@@ -1,45 +1,47 @@
-import { useEffect, useLayoutEffect, useRef, useState } from "react";
-import { View, Text, FlatList, TouchableOpacity, RefreshControl, Alert } from "react-native";
-import { Ionicons } from '@expo/vector-icons'; 
+import { useEffect, useState } from "react";
+import { View, FlatList, Text, RefreshControl, Alert, StyleSheet, TouchableOpacity } from "react-native";
 import Loading from "../../components/elements/Loading";
-import $api from "../../http";
-import { useAuth } from "../../contexts/AuthContext";
-import { useSocket } from "../../contexts/SocketContext";
 import axios from "axios";
 import INews from "../../models/Response/INews";
 import News from "../../components/News";
+import { primaryColor } from "../../constants/Colors";
 
 export default function NewsScreen ({navigation}: any) {
-  const socket = useSocket();
-  const { userId } = useAuth();
   const [isLoading, setIsLoading] = useState(true);
   const [refreshing, setRefreshing] = useState(false);
   const [news, setNews] = useState<INews[]>();
   
   //old c5703e2bb836463f9d54165908fd9a6d
   //new 7ae77478644f4a489c2f9be9b196e7a3
-  const apiKey = '7ae77478644f4a489c2f9be9b196e7a3';
+  //another b4e83102c5f4471ca03b4d20a316eee7
+  const apiKey = 'b4e83102c5f4471ca03b4d20a316eee7';
   const apiUrl = 'https://newsapi.org/v2/everything';
   const query = 'crypto';
 
+  useEffect(() => {
+    //fetchNews();
+  }, [refreshing]);
 
-  axios.get(apiUrl, {
-    params: {
-      q: query,
-      apiKey: apiKey,
-      page: 1
-    },
-  })
-    .then(response => {
-      setNews(response.data.articles);
-      setIsLoading(false);
+  const fetchNews = async() => {
+    axios.get(apiUrl, {
+      params: {
+        q: query,
+        apiKey: apiKey,
+        page: 2
+      },
     })
-    .catch(error => {
-      console.error('Ошибка при получении новостей:', error.message);
-      if(error.response.status == 429) Alert.alert('Ошибка сервера', 'Привышен лимит запросов!');
-      else Alert.alert('Непредвиденная ошибка!', 'Уже чиним 🧑‍🔧');
-      setIsLoading(false);
-    })
+      .then(response => {
+        setNews(response.data.articles);
+        setIsLoading(false);
+        setRefreshing(false);
+      })
+      .catch(error => {
+        console.error('Ошибка при получении новостей:', error.message);
+        if(error.response.status == 429) Alert.alert('Ошибка сервера', 'Привышен лимит запросов!');
+        else Alert.alert('Непредвиденная ошибка!', 'Уже чиним 🧑‍🔧');
+        setIsLoading(false);
+      })
+  }
 
   const onRefresh = () => {
     setRefreshing(true);
@@ -53,16 +55,50 @@ export default function NewsScreen ({navigation}: any) {
 
   return (
     <View>
-      <FlatList
-      data={news}
-      keyExtractor={(item, index) => index.toString()}
-      renderItem={({ item, index }) => <News key={index} news={item} />}
-      contentContainerStyle={{ padding: 10, alignItems: 'center' }}
-      showsVerticalScrollIndicator={true}
-      refreshControl={
-        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-      }
-    />
+      {news ? 
+          <FlatList
+          data={news?.map((news, index) => ({ key: index, news: news }))}
+          keyExtractor={(item) => item.key.toString()}
+          renderItem={({ item }) => <News news={item.news} />}
+          contentContainerStyle={{ padding: 10, alignItems: 'center' }}
+          showsVerticalScrollIndicator={true}
+          refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+            }
+          />
+        : 
+        <View style={styles.container}>
+          <Text style={{ 
+            textAlign: 'center', 
+            fontSize: 20, 
+            marginTop: 10}}>Новостей нет</Text>
+          <TouchableOpacity
+            onPress={() => fetchNews()}
+            style={styles.pressable}>
+            <Text style={styles.btnText}>Обновить</Text>
+          </TouchableOpacity>
+        </View>}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    gap: 20,
+    flexDirection: 'column'
+  },
+  pressable: {
+    backgroundColor: primaryColor,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    width: 'auto',
+    height: 'auto',
+    borderRadius: 25
+  },
+  btnText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: '500'
+  }
+});
